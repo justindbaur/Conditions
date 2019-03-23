c520d46a6acc296d23a85c8821215f0952468743
justindbaur <136baur@gmail.com>
2019-03-23 12:46:06 -0400

    Fixed cancel button on condition view

--- ConditionsPrototype/ConditionView.Designer.cs
+++ ConditionsPrototype/ConditionView.Designer.cs
@@ -153,6 +153,7 @@
             this.btnCancel.TabIndex = 11;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // cbConnector
             // 
--- ConditionsPrototype/ConditionView.cs
+++ ConditionsPrototype/ConditionView.cs
@@ -106,5 +106,11 @@ namespace ConditionsPrototype
             this.DialogResult = DialogResult.OK;
             this.Close();
         }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
     }
 }
